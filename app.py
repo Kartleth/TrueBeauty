fd5135ebc4f0b4ce429af443a9188ce1f8049e0c
Kartleth <Karlaarlethlerma@gmail.com>
2022-10-28 21:55:56 -0700

    Agregar hojas de estilo inicio, login,agendar_cita, base, e inicio

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ app.secret_key = 'lwiu74dhn2SuF3j'
 def hello_world():  # put application's code here
     return render_template("prueba.html")
 
+@app.route('/login')
+def inicio_sesion():  # put application's code here
+    return render_template("login.html")
+
+@app.route('/inicio')
+def inicio():  # put application's code here
+    return render_template("inicio.html")
+
+@app.route('/agendar_cita')
+def agendar_cita():  # put application's code here
+    return render_template("agendar_cita.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
