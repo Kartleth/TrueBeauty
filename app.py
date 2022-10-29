58da14e26ba103740ec27701ef12a220c21008fc
LUIS021201 <luisernestohernandez1@hotmail.com>
2022-10-28 18:59:03 -0700

    Integrar prueba.html a app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for, abort
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("prueba.html")
 
 
 if __name__ == '__main__':
